18967014a7c2b33b36da88e2ac39d8c23e6536d1
Ros3nV <99356668+Ros3nV@users.noreply.github.com>
2022-04-06 19:14:39 +0300

    Create Date.cpp

---
+++ Seminars/Dates/Date.cpp
@@ -0,0 +1,25 @@
+#include "DateClass.h"
+
+int main()
+{
+	DateClass Date1, Date2;
+	
+	Date1.setDay(31);
+	Date1.setMonth(3);
+	Date1.setYear(1916);
+	Date1.setDayOfWeek(7);
+
+	Date2.setDay(15);
+	Date2.setMonth(4);
+	Date2.setYear(1916);
+	Date2.setDayOfWeek(2);
+
+
+// Oт обекта от който викаме функцията се взема избрания ден за седмица, а от обекта който е извикан като аргумент се взима датата
+	Date1.getNewDayOfWeek(Date2);
+
+
+//Връща обект от тип DateClass	
+	DateClass Date3 = Date1.getNextDay();
+	Date3.printDate();
+}
