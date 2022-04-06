2ed5674de3ad33e62b1381c718f4758d3b3bbad5
Ros3nV <99356668+Ros3nV@users.noreply.github.com>
2022-04-06 19:33:20 +0300

    Update Date.cpp

--- Seminars/Dates/Date.cpp
+++ Seminars/Dates/Date.cpp
@@ -16,10 +16,13 @@ int main()
 
 
 // Oт обекта от който викаме функцията се взема избрания ден за седмица, а от обекта който е извикан като аргумент се взима датата
-	Date1.getNewDayOfWeek(Date2);
-
+	std::cout<<Date1.getNewDayOfWeek(Date2) << std::endl << std::endl;
 
 //Връща обект от тип DateClass	
 	DateClass Date3 = Date1.getNextDay();
 	Date3.printDate();
+	
+	DateClass Date4 = Date3;
+
+	std::cout << std::endl << Date3.equalDates(Date4)<<<< std::endl;;
 }
